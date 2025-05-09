28c2b4a28db6aefd14a31f51bdadbcb394315268
tamilatiurina <tiurinatamila@gmail.com>
2025-05-09 11:49:21 +0200

    3rd commit

--- TavernSystem.Application/TavernSystemService.cs
+++ TavernSystem.Application/TavernSystemService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using System.Text.RegularExpressions;
+using Microsoft.Data.SqlClient;
 using TavernSystem.Models;
 
 namespace TavernSystem.Application;
@@ -48,21 +49,48 @@ public class TavernSystemService: ITavernSystemService
 
     public bool AddAdventurer(Adventurer adventurer)
     {
-        const string insertString = "INSERT INTO Adventurer (Nickname, RaceId, ExperienceId, PersonId) VALUES (@Nickname, @RaceId, @ExperienceId, @PersonId)";
-
         int countRowsAdded = -1;
-        
-        using (SqlConnection connection = new SqlConnection(_connectionString))
+        if (IsValidPersonId(adventurer.PersonId))
         {
-            SqlCommand command = new SqlCommand(insertString, connection);
-            command.Parameters.AddWithValue("@Nickname", adventurer.Nickname);
-            command.Parameters.AddWithValue("@RaceId", adventurer.RaceId);
-            command.Parameters.AddWithValue("@ExperienceId", adventurer.ExperienceId);
-            command.Parameters.AddWithValue("@PersonId", adventurer.PersonId);
-            connection.Open();
-            countRowsAdded = command.ExecuteNonQuery();
+
+            const string insertString =
+                "INSERT INTO Adventurer (Nickname, RaceId, ExperienceId, PersonId) VALUES (@Nickname, @RaceId, @ExperienceId, @PersonId)";
+            
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(insertString, connection);
+                command.Parameters.AddWithValue("@Nickname", adventurer.Nickname);
+                command.Parameters.AddWithValue("@RaceId", adventurer.RaceId);
+                command.Parameters.AddWithValue("@ExperienceId", adventurer.ExperienceId);
+                command.Parameters.AddWithValue("@PersonId", adventurer.PersonId);
+                connection.Open();
+                countRowsAdded = command.ExecuteNonQuery();
+            }
         }
-        
+
         return countRowsAdded != -1;
+    }
+    
+    public static bool IsValidPersonId(string personId)
+    {
+        if (personId == null || personId.Length != 16)
+            return false;
+        
+        var regex = new Regex(@"^[A-Z]{2}(\d{4})(\d{2})(\d{2})(\d{4})[A-Z]{2}$");
+        var match = regex.Match(personId);
+
+        if (!match.Success)
+            return false;
+
+        int year = int.Parse(match.Groups[1].Value);
+        int month = int.Parse(match.Groups[2].Value);
+        int day = int.Parse(match.Groups[3].Value);
+        int noise = int.Parse(match.Groups[4].Value);
+
+        return year >= 1 && year <= 9999 &&
+               month >= 1 && month <= 11 &&
+               day >= 1 && day <= 28 &&
+               noise >= 0 && noise <= 9999;
     }
 }
